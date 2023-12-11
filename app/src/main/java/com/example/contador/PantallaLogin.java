3bb49b2d54560567db4e097976b886b79cdb394b
saul <saulal86@educastur.es>
2023-12-11 18:25:46 +0100

    boton de esconder la contraseña

--- app/src/main/java/com/example/contador/PantallaLogin.java
+++ app/src/main/java/com/example/contador/PantallaLogin.java
@@ -25,42 +25,12 @@ public class PantallaLogin extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_login);
-
         username = findViewById(R.id.username1);
         password = findViewById(R.id.password1);
         botonlogin = findViewById(R.id.botonsignin1);
-        togglePasswordVisibility = findViewById(R.id.togglePasswordVisibility);  // Agregado
         DB = new DataBaseHelper(this);
 
-        // Agregado para gestionar la visibilidad de la contraseña
-        togglePasswordVisibility.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                int inputType = isChecked ?
-                        android.text.InputType.TYPE_CLASS_TEXT :
-                        android.text.InputType.TYPE_CLASS_TEXT | android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD;
-
-                password.setInputType(inputType);
-                password.setSelection(password.length());
-            }
-        });
-
         // Agregado: TextWatcher para mantener el estado del interruptor actualizado
-        password.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                boolean passwordVisible = (password.getInputType() & android.text.InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) > 0;
-                togglePasswordVisibility.setChecked(passwordVisible);
-            }
-        });
 
         botonlogin.setOnClickListener(new View.OnClickListener() {
             @Override
